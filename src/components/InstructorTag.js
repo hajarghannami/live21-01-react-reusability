489bc08c01202ad6504cbe1e1a4ba6489a82c439
hajarghannami <hajarghannami@outlook.com>
2021-01-24 10:34:05 +0300

    hey

--- src/components/InstructorTag.js
+++ src/components/InstructorTag.js
@@ -1,7 +1,18 @@
-const InstructorTag = () => {
-  // Create the InstructorTag component here
+import instructorData from "../instructorData";
+import styled from "styled-components";
+import { TagWrapper, Emoji, Name, GoToGithub } from "./styles";
 
-  return <></>;
+const InstructorTag = () => {
+  const instructorTag = instructorData.map((instructor) => {
+    return (
+      <TagWrapper onClick={() => window.open(`{instructor.github}`)}>
+        <Emoji>{instructor.emoji}</Emoji>
+        <Name>{instructor.name}</Name>
+        <GoToGithub>{instructor.description}</GoToGithub>
+      </TagWrapper>
+    );
+  });
+  return instructorTag;
 };
 
 export default InstructorTag;
---
+++ src/instructorData.js
@@ -0,0 +1,25 @@
+const instructorData = [
+  {
+    id: 1,
+    emoji: "🌚",
+    name: "Hamza",
+    description: "Go to GitHub",
+    github: "https://github.com/DarthHamza",
+  },
+  {
+    id: 2,
+    emoji: "🐥",
+    name: "Laila",
+    description: "Go to GitHub",
+    github: "https://github.com/Lailz",
+  },
+  {
+    id: 3,
+    emoji: "🦍",
+    name: "Hasan",
+    description: "Go to GitHub",
+    github: "https://github.com/thehasanas",
+  },
+];
+
+export default instructorData;
