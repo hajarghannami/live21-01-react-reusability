f89a6a1bd5fb1055c8ce10d724cb4fa9ede9b86a
hajarghannami <hajarghannami@outlook.com>
2021-01-25 08:13:42 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
 import InstructorTag from "./components/InstructorTag";
+import instructorData from "./instructorData";
 import { AppWrapper } from "./components/styles";
+
+export const InstructorList = () => {
+  const instructors = instructorData.map((oneInstructor) => (
+    <InstructorTag
+      key={oneInstructor.id}
+      name={oneInstructor.name}
+      github={oneInstructor.github}
+      emoji={oneInstructor.emoji}
+      description={oneInstructor.description}
+    />
+  ));
+
+  return <div>{instructors}</div>;
+};
 
 const App = () => {
   return (
     <AppWrapper>
       <h2>When in doubt, ask for help!</h2>
-      <InstructorTag />
+      <InstructorList />
     </AppWrapper>
   );
 };
--- src/components/InstructorTag.js
+++ src/components/InstructorTag.js
@@ -1,18 +1,13 @@
-import instructorData from "../instructorData";
-import styled from "styled-components";
 import { TagWrapper, Emoji, Name, GoToGithub } from "./styles";
 
-const InstructorTag = () => {
-  const instructorTag = instructorData.map((instructor) => {
-    return (
-      <TagWrapper onClick={() => window.open(`{instructor.github}`)}>
-        <Emoji>{instructor.emoji}</Emoji>
-        <Name>{instructor.name}</Name>
-        <GoToGithub>{instructor.description}</GoToGithub>
-      </TagWrapper>
-    );
-  });
-  return instructorTag;
+const InstructorTag = (props) => {
+  return (
+    <TagWrapper onClick={() => window.open(`${props.github}`)}>
+      <Emoji>{props.emoji}</Emoji>
+      <Name>{props.name}</Name>
+      <GoToGithub>{props.description}</GoToGithub>
+    </TagWrapper>
+  );
 };
 
 export default InstructorTag;
